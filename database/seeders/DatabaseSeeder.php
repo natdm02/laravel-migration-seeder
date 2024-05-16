151b6b864c6a30e74145f3b21d638217719f5f21
natdm02 <natalidimarino2002@gmail.com>
2024-05-17 00:28:46 +0200

    [add] continuous

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,32 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+use App\Models\Trains;
+use Faker\Generator as Faker;
+class TrainsSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run(Faker $faker)
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for ($i = 0; $i < 20; $i++) {
+            $train = new Train();
+            $train->company = $faker->company();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->time();
+            $train->arrival_time = $faker->time();
+            $train->train_code = $faker->unique()->randomNumber();
+            $train->carriage_number = $faker->randomNumber();
+            $train->on_time = $faker->boolean();
+            $train->canceled = $faker->boolean();
+            $train->save();
+        }
     }
 }
