043a7397650329c71c38327674e764338a2a4693
natdm02 <natalidimarino2002@gmail.com>
2024-05-17 00:26:04 +0200

    [add] completed

---
+++ database/migrations/2023_05_25_145931_create_trains_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('company')->nullable();
+            $table->string('departure_station')->nullable();
+            $table->string('arrival_station')->nullable();
+            $table->time('departure_time')->nullable();
+            $table->time('arrival_time')->nullable();
+            $table->string('train_code')->nullable();
+            $table->integer('carriage_number')->unsigned()->nullable();
+            $table->boolean('on_time')->default(true);
+            $table->boolean('canceled')->default(false);
+            $table->timestamps();
+            });
+        
+        
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
