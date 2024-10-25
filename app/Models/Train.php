5f50b3841f0b4641191cf48d3087429bdb371944
natdm02 <natalidimarino2002@gmail.com>
2024-10-25 15:38:00 +0200

    [add] created Train model

---
+++ app/Models/Train.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Train extends Model
+{
+    use HasFactory;
+
+    protected $table = 'trains';
+
+    protected $fillable = [
+        'azienda',
+        'stazione_partenza',
+        'stazione_arrivo',
+        'orario_partenza',
+        'orario_arrivo',
+        'codice_treno',
+        'numero_carrozze',
+        'in_orario',
+        'cancellato',
+    ];
+}
