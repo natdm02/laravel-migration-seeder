f3182815cf9ab92a6ba5a40fca22bf246c0dcfe3
natdm02 <natalidimarino2002@gmail.com>
2024-10-28 12:07:00 +0100

    [add] passenger model creation, and controller

--- database/migrations/2024_10_25_131959_create_trains_table.php
+++ database/migrations/2024_10_25_131959_create_trains_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->string('azienda');
             $table->string('stazione_partenza');
             $table->string('stazione_arrivo');
-            $table->time('orario_partenza');
-            $table->time('orario_arrivo');
+            $table->dateTime('orario_partenza');
+            $table->dataTime('orario_arrivo');
             $table->string('codice_treno')->unique();
             $table->integer('numero_carrozze');
             $table->boolean('in_orario')->default(true);
