a20d30dc35e97c3f3fa893a5879bfc078b049de5
natdm02 <natalidimarino2002@gmail.com>
2024-10-28 12:22:46 +0100

    [add] implementation  [add] passenger table creation

--- app/Http/Controllers/PassengerController.php
+++ app/Http/Controllers/PassengerController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Passenger;
 use Illuminate\Http\Request;
 
 class PassengerController extends Controller
+
 {
-    //
+    public function index()
+    {
+        $passengers = Passenger::all();
+        return view('passengers.index', compact('passengers'));
+    }
+
+
 }
